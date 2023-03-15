edc32b7bfd2b6a2d81d99077ca7b29d37bdd1cc2
Raul <alex_lion345@hotmail.com>
2023-03-15 15:39:01 -0600

    cambie las coordenadas en x, pero en y siguen estando mal

--- LineFollower.py
+++ LineFollower.py
@@ -30,13 +30,24 @@ value_threshold = 50 # valor mínimo de brillo
 
 ################################### Esto es para la cuestión del cambio de coordenadas##########
 
-# Definir las coordenadas del rectángulo
+# Definir las coordenadas del rectángulo (en terminos de la pantalla)
 x1, y1 = 10, 10  # esquina superior izquierda
 x2, y2 = 310, 230  # esquina inferior derecha
 
-# Definir las coordenadas del rectángulo fijo
+# Definir las coordenadas del rectángulo fijo (en terminos de la grafica)
 rect_top_left = (0, 2)
 rect_bottom_right = (10, -2)
+
+# Tamaño del rectangulo 
+rect_top_left_coord_originales = (0,0)
+rect_bottom_right_coord_originales = (300,220)
+#Definir la relación
+a = (rect_bottom_right[0] - rect_top_left[0]) / (rect_bottom_right_coord_originales[0] - rect_top_left_coord_originales[0])
+b = rect_top_left_coord_originales[1]
+
+c = (rect_bottom_right[1] - rect_top_left[1]) / (rect_bottom_right_coord_originales[1]-rect_top_left_coord_originales[1]) 
+d = rect_top_left[1]
+
 
 ################################# Esto es para leer la cámara #################################
 
@@ -56,12 +67,14 @@ if __name__=='__main__':
             # Convierte el color a HSV
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             
-            # Aquí se define el color azul rn el espacio de HSV
-            # lower = np.array([60,100,100])
-            # upper = np.array([180,255,255])
+            # Aquí se define el color azul rn el espacio de HSV (es util para hacer pruebas
+            # ya que es más fácil conseguir un objeto de color azul y manipularlo)
+            lower = np.array([60,100,100])
+            upper = np.array([180,255,255])
                     
-            lower = np.array([hsv_color[0] - hue_tolerance, saturation_threshold, value_threshold])
-            upper = np.array([hsv_color[0] + hue_tolerance, 255, 255])      
+            # Aquí se define el color que se estableció en formato hexadecimal
+            # lower = np.array([hsv_color[0] - hue_tolerance, saturation_threshold, value_threshold])
+            # upper = np.array([hsv_color[0] + hue_tolerance, 255, 255])      
 
             # LIMITA la imagen para solo mostrar el azul
             mask = cv2.inRange(hsv, lower, upper)
@@ -75,7 +88,7 @@ if __name__=='__main__':
             contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
             # Dibuja un rectangulo para poder enmarcar allí lo que deseo
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(res, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
             # Dibuja un rectangulo en la línea que quiero monitorear
             if len(contours) > 0:
@@ -87,11 +100,20 @@ if __name__=='__main__':
 
                 # Dibuja un rectángulo alrededor del contorno
                 cv2.rectangle(res, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
+                
+                # Encuentra el centro
                 centro_x=x+w/2
                 centro_y=y+h/2
+
+                # Restar las coordenadas del rectángulo fijo para obtener las coordenadas relativas
+                rel_x = centro_x - x1
+                rel_y = centro_y - y2
+
+                # Cálculo para las nuevas coordenadas en relación a la gráfica
+                x_g = a*rel_x+b
+                y_g = c*rel_y+d
                 # Muestra las coordenadas del objeto azul en la ventana
-                print(f"Coordenadas (x, y) del centro del objeto azul: ({centro_x}, {centro_y})")
+                print(f"Coordenadas (x, y) del centro del objeto azul: ({x_g}, {y_g})")
 
             cv2.imshow('Imagen Original', frame)
             cv2.imshow('Detección de color azul', res)
